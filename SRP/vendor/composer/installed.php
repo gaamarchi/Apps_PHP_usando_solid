d7d2ff4dbc031ed5ee1da62a01afc5ad8e45cb9e
gabriel marchi <101679723+gaamarchi@users.noreply.github.com>
2023-11-16 16:53:13 -0300

    realocate folders

---
+++ SRP/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lotus/srp',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5f5f2e417629f80689124d9b208d032ae1a8a61a',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lotus/srp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5f5f2e417629f80689124d9b208d032ae1a8a61a',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
